98ada135edcaa1eb0bbc22857f90f066adb6e764
jwiktorowicz <jwiktorowicz@student.agh.edu.pl>
2024-01-06 23:05:49 +0100

    refaktoryzacja pętli for

--- siec/src/reports.cpp
+++ siec/src/reports.cpp
@@ -2,6 +2,8 @@
 #include "nodes.hpp"
 #include <iostream>
 #include <string>
+#include <algorithm>
+#include <vector>
 
 void generate_structure_report(const Factory& f, std::ostream& os){
     os <<"\n== LOADING RAMPS ==" << std::endl;
@@ -10,10 +12,10 @@ void generate_structure_report(const Factory& f, std::ostream& os){
         os << std::endl << "LOADING RAMP #" << rmp.get_id() << std::endl;
         os << "  Delivery interval: " << rmp.get_delivery_interval() << std::endl;
         os << "  Receivers:" << std::endl;
-        int i = 1;
+        int worker_counter = 1;
         for (auto it = rmp.receiver_preferences_.begin(), end_it = rmp.receiver_preferences_.cend(); it != end_it; ++it){
-            os << "    worker #" << i << std::endl;
-            ++i;
+            os << "    worker #" << worker_counter << std::endl;
+            ++worker_counter;
         }
     };
     std::for_each(f.ramp_cbegin(), f.ramp_cend(), ramp);
@@ -33,13 +35,16 @@ void generate_structure_report(const Factory& f, std::ostream& os){
         os << "\n  Queue type:" << s << std::endl;
 
         os << "  Receivers:" << std::endl;
-
+        int worker_counter = 1;
+        int storehouse_counter = 1;
         for (auto &receiver: wrkr.receiver_preferences_){
             if (receiver.first->get_receiver_type() == ReceiverType::WORKER){
-                os << "    worker #" << receiver.first->get_id() << std::endl;
+                os << "    worker #" << worker_counter << std::endl;
+                ++worker_counter;
             }
             else {
-                os << "    storehouse #" << receiver.first->get_id() << std::endl;
+                os << "    storehouse #" << storehouse_counter << std::endl;
+                ++storehouse_counter;
             }
         }
     };
@@ -59,19 +64,27 @@ void generate_simulation_turn_report(const Factory& f, std::ostream& os, Time t)
 
     os << "== WORKERS ==" << std::endl;
 
-    auto worker = [&os] (const Worker &wrkr) {
-        os << std::endl << "WORKER #" << wrkr.get_id() << std::endl;
+    std::vector<ElementID> workerz_id;
+    for (auto it = f.worker_cbegin(); it != f.worker_cend(); ++it){
+        workerz_id.push_back(it->get_id());
+    }
+    std::sort(workerz_id.begin(), workerz_id.end());
 
-        auto &processed_buffer = wrkr.get_processing_buffer();
-        if (processed_buffer != std::nullopt) {
-            os << "  PBuffer: #" << processed_buffer->get_id() << " (pt = " << wrkr.get_processing_duration() - wrkr.get_package_processing_start_time() << ")"
-               << std::endl;
-        } else {
+
+    for (ElementID id: workerz_id){
+        auto current_worker = f.find_worker_by_id(id);
+        os << std::endl << "WORKER #" << (*current_worker).get_id() << std::endl;
+
+        auto &processed_buffer = (*current_worker).get_processing_buffer();
+        Time processing_time = (t - (*current_worker).get_package_processing_start_time() + 1) % (*current_worker).get_processing_duration() ;
+        if (processed_buffer == std::nullopt or processed_buffer->get_id() == 0 or processing_time <= 0) {
             os << "  PBuffer: (empty)" << std::endl;
-
+        } else {
+            os << "  PBuffer: #" << processed_buffer->get_id() << " (pt = " << processing_time << ")"
+               << std::endl;
         }
 
-        const auto &queue = wrkr.get_queue();
+        const auto &queue = (*current_worker).get_queue();
         os << "  Queue: ";
         if (queue->cbegin() != queue->cend()) {
             for (const auto &package: *queue) {
@@ -85,17 +98,14 @@ void generate_simulation_turn_report(const Factory& f, std::ostream& os, Time t)
             os << "(empty)" << std::endl;
         }
 
-        auto& sent_buffer = wrkr.get_sending_buffer();
-        if (sent_buffer != std::nullopt) {
-            os << "  SBuffer: #" << sent_buffer->get_id() << std::endl;
-
-        } else {
+        auto& sent_buffer = (*current_worker).get_sending_buffer();
+        if (sent_buffer == std::nullopt or sent_buffer->get_id() <= 0) {
             os << "  SBuffer: (empty)" << std::endl;
+        } else {
+            os << "  SBuffer: #" << sent_buffer->get_id() << std::endl;
         }
-
-
-    };
-        std::for_each(f.worker_cbegin(), f.worker_cend(), worker);
+    }
+    
 
 
         os << std::endl << std::endl << "== STOREHOUSES ==" << std::endl;
